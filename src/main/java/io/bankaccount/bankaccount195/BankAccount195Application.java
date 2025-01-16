9723837da25f3f6b23c099f01059ba20f1a904ba
uxoa <palomita@gmail.com>
2025-01-16 10:57:52 +0100

    first commit

---
+++ src/main/java/io/bankaccount/bankaccount195/BankAccount195Application.java
@@ -0,0 +1,13 @@
+package io.bankaccount.bankaccount195;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankAccount195Application {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(BankAccount195Application.class, args);
+    }
+    
+}
